058454c3fc729cd8ecbd1dc1a5dfae5c1a50fa34
JIN JU YOO <102593738+yyypearl@users.noreply.github.com>
2024-06-24 23:37:04 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.64 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,12 +1,10 @@
 function solution(numbers) {
     var result = [];
-    var k=0;
     for(let i=0; i<numbers.length; i++){
         for (j=i+1; j<numbers.length; j++){
-            result[k] = numbers[i]+numbers[j];
-            k++;
+            result.push(numbers[i]+numbers[j]);
         }
     }
-    const answer = result.filter((element, index) => result.indexOf(element) === index).sort((a,b) => a-b);
+    const answer = [...new Set(result)].sort((a,b) => a-b);
     return answer;
 }
